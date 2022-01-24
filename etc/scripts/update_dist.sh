b5b2e4a3404239eff214decab24d4a6b1c91e1fb
slmnv5 <slmnv5@gmail.com>
2022-01-24 00:35:21 -0500

    1

--- etc/scripts/update_dist.sh
+++ etc/scripts/update_dist.sh
@@ -3,7 +3,8 @@
 cd ~/pepelats || exit 1
 python3 -O -m PyInstaller --add-data="etc:etc" --add-data="start.sh:." \
   --add-data="README.md:." --name=pepelatsexe \
-  --add-binary="/usr/lib/arm-linux-gnueabihf/libportaudio.so.2:." \
+  --add-binary="/usr/lib/arm-linux-gnueabihf/libportaudio.so.2:./libportaudio.so" \
+  --add-binary="/usr/lib/arm-linux-gnueabihf/libsndfile.so.1:./libsndfile.so" \
   --hidden-import="mido.backends.rtmidi" --noconfirm start.py
 
 rsync -av --progress --exclude=".git" --exclude=".gitignore" \
