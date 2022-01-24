686e735d1d218496c53d160764fa36641347a099
slmnv5 <slmnv5@gmail.com>
2022-01-23 23:27:17 -0500

    1

--- etc/scripts/update_dist.sh
+++ etc/scripts/update_dist.sh
@@ -3,13 +3,17 @@
 cd ~/pepelats || exit 1
 python3 -O -m PyInstaller --add-data="etc:etc" --add-data="start.sh:." \
   --add-data="README.md:." --name=pepelatsexe \
+  --add-binary="/usr/lib/arm-linux-gnueabihf/libportaudio.so.2"
   --hidden-import="mido.backends.rtmidi" --noconfirm start.py
 
 rsync -av --progress --exclude=".git" --exclude=".gitignore" \
   --exclude="save_song" --delete ./dist/pepelatsexe/ ~/pepelatsexe
 
+# /usr/lib/arm-linux-gnueabihf/libportaudio.so.2
+
 cd ~/pepelatsexe || exit 1
 git status
+git reset --hard
 git add --all
 git commit -m"1"
 git push
