fe45f94f7d8357ef2ee9eb96fbaf2bff9fc36ce5
slmnv5 <slmnv5@gmail.com>
2022-04-17 00:03:21 -0400

    1

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -8,18 +8,11 @@ from utils import JsonDictLoader, ConfigName
 class TestLoader(TestCase):
 
     def test_1(self):
-        loader = JsonDictLoader("./etc/count/kbd_notes.json")
+        loader = JsonDictLoader("./etc/looper_defaults.json")
         file: Path = loader.get_filename()
-        self.assertTrue(file.name == "kbd_notes.json")
+        self.assertTrue(file.name == "looper_defaults.json")
         note: int = loader.get(ConfigName.kbd_notes, dict()).get("1", None)
         self.assertTrue(note == 60)
-
-    def test_2(self):
-        loader = JsonDictLoader("./etc/count/mapped_notes.json")
-        file: Path = loader.get_filename()
-        self.assertTrue(file.name == "mapped_notes.json")
-        note: int = loader.get(ConfigName.mapped_notes, dict()).get("60", None)
-        self.assertTrue(note == 80)
 
 
 if __name__ == "__main__":
