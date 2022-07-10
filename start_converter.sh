e5f1d8ae80c1ed971245445adb5564d014790c80
slmnv5 <slmnv5@gmail.com>
2022-07-10 01:50:30 -0400

    1

--- start_converter.sh
+++ start_converter.sh
@@ -19,28 +19,36 @@ check_if_running() {
   fi
 }
 
+download_file() {
+if [[ ! -f $1  || -s $1 ]]; then
+  wget -O mimap5 https://github.com/slmnv5/mimap5/blob/master/mimap5?raw=true
+  if [[ ! -f $1 || -s $1 ]]; then
+    echo "Error downloading file $1"
+    exit 1
+  fi
+fi
+}
+
+
 cd_to_script_dir
 check_if_running
+download_file(mimap5)
+download_file(rules.txt)
+download_file(kbdmap.txt)
 
-if [[ ! -f rules.txt ]]; then
-  wget -O rules.txt https://github.com/slmnv5/mimap5/raw/master/rules.txt
-  if [[ ! -f rules.txt ]]; then
-    echo "Error downloading rules.txt"
-    exit 1
-  fi
-fi
 
-if [[ ! -f mimap5 ]]; then
-  wget -O mimap5 https://github.com/slmnv5/mimap5/blob/master/mimap5?raw=true
-  if [[ ! -f mimap5 ]]; then
-    echo "Error downloading mimap5"
-    exit 1
-  fi
-  chmod a+x mimap5
-fi
+chmod a+x mimap5
+
+
+
+
+# Start converter and create in and out virtual MIDI ports
+sudo ./mimap5 -r rules.txt -n "$EXT_CONV" -k kbdmap.txt &
+time sleep 2
+
 
 HARDWARE_OUT=""
-while true; do
+while k in {1..10}; do
   echo "Waiting for MIDI port $HARDWARE_NAME"
   HARDWARE_OUT=$(aconnect -l | awk -v nm="$HARDWARE_NAME" '$0 ~ nm {print $2;exit}')
   if [ -z "$HARDWARE_OUT" ]; then
@@ -50,15 +58,9 @@ while true; do
   fi
 done
 
-# Start converter and create in and out virtual MIDI ports
-./mimap5 -r rules.txt -n "$EXT_CONV" &
-time sleep 2
-
 CLIENT_IN=$(aconnect -l | awk -v nm="$EXT_CONV" '$0 ~ nm {print $2;exit}')
 if aconnect -e "${HARDWARE_OUT}0" "${CLIENT_IN}0"; then
   echo "Connected MIDI $HARDWARE_OUT to  $EXT_CONV"
-  exit 0
 else
   echo "Failed connect MIDI $HARDWARE_OUT to  $EXT_CONV"
-  exit 1
 fi
